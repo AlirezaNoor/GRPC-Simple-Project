b87f9dcb7b13499c0bbcb865852a449fd3815a02
AlirezaNoor <alirezang7575@gmail.com>
2024-06-10 04:06:12 -0400

    init files

---
+++ GrpcClinet/GrpcClinet/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        
+        /// baryae nadide gerftan ssl
+        var httpHandler = new HttpClientHandler();
+        httpHandler.ServerCertificateCustomValidationCallback = 
+            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+        // ساخت کانال gRPC
+        using var channel = GrpcChannel.ForAddress("https://localhost:7166", new GrpcChannelOptions { HttpHandler = httpHandler });
+        var client = new Greeter.GreeterClient(channel);
+
+        // ارسال درخواست به سرویس gRPC
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+
+        Console.WriteLine("Greeting: " + reply.Message);
+    }
+}
